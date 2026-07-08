6a70cc8fda336ab8f220b14184c3f3dfba30be77
emsrn <sabourin.emma2@gmail.com>
2026-07-08 08:35:04 +0200

    Add RFID and servo control tests
    Implement RFID initialization and servo control logic.

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-// les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
+a4_microSySTEM_Barrier.initRFID()
+a4_microSySTEM_Barrier.setServoAngle(5)
+basic.forever(function () {
+    while (a4_microSySTEM_Barrier.readID() != 9434499) {
+        a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Red)
+    }
+    a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Green)
+    a4_microSySTEM_Barrier.setServoAngle(90)
+    while (!(a4_microSySTEM_Barrier.presenceSensor())) {
+    	
+    }
+    while (a4_microSySTEM_Barrier.presenceSensor()) {
+        a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Red)
+    }
+    a4_microSySTEM_Barrier.setServoAngle(5)
+})
