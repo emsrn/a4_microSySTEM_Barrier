2e65d25f278eacce06181c0f8b229c367802e11b
emsrn <sabourin.emma2@gmail.com>
2026-07-09 09:12:27 +0200

    Remove servo and color logic from main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,1 @@
-a4_microSySTEM_Barrier.setServoAngle(5)
-basic.forever(function () {
-    while (a4_microSySTEM_Barrier.readID() != 9434499) {
-        a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Red)
-    }
-    a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Green)
-    a4_microSySTEM_Barrier.setServoAngle(90)
-    while (!(a4_microSySTEM_Barrier.presenceSensor())) {
-    	
-    }
-    while (a4_microSySTEM_Barrier.presenceSensor()) {
-        a4_microSySTEM_Barrier.setRingColor(a4_microSySTEM_Barrier.Colors.Red)
-    }
-    a4_microSySTEM_Barrier.setServoAngle(5)
-})
+
